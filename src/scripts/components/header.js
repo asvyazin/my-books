d6af922b74f5c489d44dae45884ad4ac7a2ab31a
Alexander Svyazin <guybrush@live.ru>
2015-09-20 22:15:39 +0500

    navbar

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Navbar, NavItem, Nav } from "react-bootstrap";
 
 let Header = React.createClass({
     render: function () {
-        return (
-            <div className="header">
-                <h3 className="text-muted">{this.props.title}</h3>
-            </div>
-        );
+        return <Navbar brand={this.props.title}>
+            <Nav>
+                <NavItem href="/Login">Get new access token</NavItem>
+            </Nav>
+        </Navbar>;
     }
 });
 
